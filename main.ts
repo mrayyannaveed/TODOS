f048c27c9328f5d507cdf4dce915ecd829f357ca
Rayyan Naveed <164235692+mrayyannaveed@users.noreply.github.com>
2024-04-01 23:33:08 +0500

    Pushing todo app to the github

---
+++ main.ts
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true;console.log("*****Welcome to the todo app*****");
+
+while (condition) {
+    
+let addTask = await inquirer.prompt(
+    [
+        {
+            name: "todo",
+            message: "What you want to add in your todos? ",
+            type: "input"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more? ",
+            default: "false"
+        },
+    ]
+);
+todos.push(addTask.todo);
+condition = addTask.addMore;
+console.log(todos);
+
+}
+
+if (condition === false){
+    let removeTask = await inquirer.prompt(
+        [
+            {
+                name: "removeTodo",
+                message: "Do you want to remove the last todo?",
+                type: "confirm",
+                default: "false"
+            },
+        ]
+    );
+    if(removeTask.removeTodo === true){
+        todos.pop();
+    }
+    console.log(todos);
+}
